5700ba9558ffa432404bc89fa3276acbe8e302c4
mohamed <mohamedabdelfatah2027@gmail.com>
2022-02-19 13:35:06 +0200

    added 404 not found response

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,25 @@ fn main() {
 fn handle_stream(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    let content = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 OK\r\nContent-Length: {}\r\n\r\n{}"
-                           ,content.len()
-                           ,content);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap()
+
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let status_line = "HTTP/1.1 OK";
+        let response= res_format(status_line, "hello.html");
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap()
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let response = res_format(status_line, "404.html");
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+     }
+}
+
+fn res_format(status_line: &str, content_file: &str) -> String {
+        let content = fs::read_to_string(content_file).unwrap();
+        format!("{}\r\nContent-Length: {}\r\n\r\n{}"
+                               ,status_line
+                               ,content.len()
+                               ,content)
 }
